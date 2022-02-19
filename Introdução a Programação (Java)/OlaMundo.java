0acbb356e5a829bf96b2710a7c07b482c3f97f3d
Guilherme Lelinski Cardoso <84156346+GuilhermeLelinski@users.noreply.github.com>
2022-02-18 22:43:51 -0300

    Update OlaMundo.java

--- Introdução a Programação (Java)/OlaMundo.java
+++ Introdução a Programação (Java)/OlaMundo.java
@@ -1,31 +1,11 @@
 
-package ola.mundo;
+package helloWorld;
 
 import java.util.Scanner;
 
 
-public class OlaMundo {
-
-    
+public class helloWorld {
     public static void main(String[] args) {
-        Scanner input = new Scanner (System.in);
-        
-        String nome;
-        double peso, altura, imc;
- // Entrada de dados       
-        System.out.println("Digite o seu nome:");
-        nome = input.nextLine();
-        System.out.println("Digite o seu peso:");
-        peso = Double.valueOf (input.nextLine());
-        System.out.println("Digite a sua altura:");
-        altura = Double.valueOf (input.nextLine());
- // Processamento dos dados       
-        imc = peso / (altura*altura);
-                
-       
-        System.out.println ("Olá "+nome+" esse é o seu segundo programa em Java");
-        System.out.println("Seu IMC é: "+imc);
-        
-    }
-    
+        System.out.println("Hello World!")   
+    }    
 }
